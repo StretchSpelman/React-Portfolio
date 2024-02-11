ef3431eeccd5ba74475e82aa50235ab6f5040f71
Stretch Spelman <codingspelman@gmail.com>
2024-02-11 00:41:17 -0700

    added substance to files

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -3,6 +3,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import {Link, useLocation} from 'react-router-dom';
+
+
 function OffcanvasExample() {
     const expand="sm";
     const priority = {
