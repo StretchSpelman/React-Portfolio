0157e8712d2c0f126b93eb101c1e47f272543c97
Stretch Spelman <codingspelman@gmail.com>
2024-02-13 18:15:37 -0700

    added content to pages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,12 @@
+
+import ListGroup from 'react-bootstrap/ListGroup';
+
 export default function ContactPage(){
-return
+return (
+    <ListGroup variant="flush">
+    <ListGroup.Item>Email: codingspelman@gmail.com</ListGroup.Item>
+    <ListGroup.Item>Github: https://github.com/StretchSpelman </ListGroup.Item>
+    <ListGroup.Item>X: https://twitter.com/codingspelman </ListGroup.Item>
+  </ListGroup>
+)
 }
